b1f721e8177b74f5983bbc2beb11accc45ab796c
BenjaminN117 <benjaminhewett409@gmail.com>
2023-06-21 10:37:29 +0100

    Feat/ Many modifications - Imp PyWebCopy

---
+++ src/main.py
@@ -0,0 +1,15 @@
+'''
+Product:
+Description:
+Author: Benjamin Norman 2023
+'''
+
+from aws_interactions import s3_interactions
+from detection import website_detection
+from env import *
+
+import logging
+
+
+if __name__ == "__main__":
+    pass
