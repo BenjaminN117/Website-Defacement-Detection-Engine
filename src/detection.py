fd163a203a7498d1f19bbebec6c5da3f2f10acd3
BenjaminN117 <benjaminhewett409@gmail.com>
2023-06-29 14:05:31 +0100

    Feat/Bug: Added directory cleaning for the live data. Fixed a bug on failing to remove directories

--- src/detection.py
+++ src/detection.py
@@ -9,8 +9,9 @@ import os
 import requests
 import json
 import shutil
+import hashlib
 
-from pywebcopy import save_web_page
+from pywebcopy import save_web_page, save_website
 
 from env import *
 from aws_interactions import s3_interactions, sns_interactions
@@ -28,7 +29,7 @@ class website_detection():
         - If the website has mulitple pages, download them all, these names should then be based off of the
           known good code folder. e.g. https://somethingFishy.com/index.html
         '''
-        
+
         '''
         Fetches the website data from the listed
         websites
@@ -36,42 +37,58 @@ class website_detection():
         
         builtURL = ""
         
-        os.makedirs("Data/Live_Data", exist_ok=True)
+        # os.makedirs("Data/Live_Data", exist_ok=True)
         
         # Fetch the website data for HTTPS to start with and then HTTP if not exists
         
         for item in productionDirectoryWalk["domains"]:
             for domain, value in item.items():
+                # removes one of the problems, or not.....
                 os.makedirs(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}", exist_ok=True)
-                download_folder = f'{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}'   
+                download_folder = f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}"
                 kwargs = {'bypass_robots': True}
                 
                 try:
                     try:
                         builtURL = f"https://{domain}"
                         print(builtURL)
-                        save_web_page(builtURL, download_folder, **kwargs)
+                        save_website(builtURL, download_folder, **kwargs)
                         if len(os.listdir(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}/https_{domain}")) == 0:
                             # Log to the log file
-                            os.removedirs(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}")
-                            
+                            shutil.rmtree(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}")
+                        else:
+                            self.directory_cleaning(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}/https_{domain}/{domain}", f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}/")
+                            self.directory_cleaning(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}/https_{domain}", f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}/")
+
                     except requests.exceptions.SSLError:
                         builtURL = f"http://{domain}"
                         print(builtURL)
-                        save_web_page(builtURL, download_folder, **kwargs)
+                        save_website(builtURL, download_folder, **kwargs)
                         os.removedirs(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}/https_{domain}")
                         if len(os.listdir(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}/http_{domain}")) == 0:
-                            os.removedirs(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}")
-                            
+                            shutil.rmtree(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}")
+                        else:
+                            self.directory_cleaning(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}/http_{domain}/{domain}", f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}/")
+                            self.directory_cleaning(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}/http_{domain}", f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}/")
+                # This is part of the bug
                 except Exception as err:
                     print(f"URL NOT FOUND {err}")
                     shutil.rmtree(f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{domain}", ignore_errors=True)
-    
+
+    def directory_cleaning(self, target, destination):
+        '''
+        Moves all files in a directory back one, then deletes the directory they were in
+        '''
+        
+        for file in os.listdir(target):
+            file_path = os.path.join(target, file)
+            shutil.move(file_path, destination)
+            
+        shutil.rmtree(target)
+        
     def data_cleaning(self, fileName):
         
         # Private function
-        
-        # Dependancy for web_fetcher
         
         '''
         Removes the comments added by the pywebcopy module that could interfere
@@ -79,11 +96,14 @@ class website_detection():
         
         Needs to be runon every .html file as this  is what it effects
         
+        Need to rethink this a bit, maybe use regex to search between a set number of lines then apply a removal rule
+        
         '''
         if fileName.endswith(".html"):
+
             with open(fileName, "r") as file:
                 lines = file.readlines()
-
+                
             lines_to_remove = [2, 3, 4, 5, 6]
             filtered_lines = [line for i, line in enumerate(lines) if i+1 not in lines_to_remove]
             
@@ -110,10 +130,27 @@ class website_detection():
           text file like an image, then do a hash function comparison
         - if the file does not exist in production then add it into the report or filenames that aren't there
         
+        First look at the files in the live directory, compare those to the paths and filenames of the production directory, for every match do a comparison
+        and for every difference add it to the output file as an unknown file
+        
         '''
-
-        pass
-    
+        
+        # import the dict of known text file extensions that need comparison and not hashing
+        with open("file_extensions.json", "r") as data:
+            fileExtensions = json.load(data)
+            
+        
+        for item in liveDirectoryWalk["domains"]:
+            for key, value in item.items():
+                for x in value:
+                    for nkey, nvalue in x.items():
+                        print(nkey, nvalue)
+                        
+        for item in productionDirectoryWalk["domains"]:
+            for key, value in item.items():
+                for x in value:
+                    for nkey, nvalue in x.items():
+                        print(nkey, nvalue)
     
     def dir_walker(self, targetFilePath):
 
@@ -149,15 +186,12 @@ class website_detection():
         return domainWalk
                 
 
-    
 if __name__ == "__main__":
     inst = website_detection()
     
     productionDirectoryWalk = inst.dir_walker(PRODUCTION_WEBSITES_DOWNLOAD_LOCATION)
-    # print(productionDirectoryWalk)
-    # inst.web_fetcher(productionDirectoryWalk)
+    inst.web_fetcher(productionDirectoryWalk)
     liveDirectoryWalk = inst.dir_walker(LIVE_WEBSITES_DOWNLOAD_LOCATION)
-    #print(liveDirectoryWalk)
     
     
     ### Data cleaning ###
@@ -165,7 +199,11 @@ if __name__ == "__main__":
         for key, value in item.items():
             for x in value:
                 for nkey, nvalue in x.items():
-                    inst.data_cleaning(fileName=f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{nvalue}")
-    
+                    try:
+                        inst.data_cleaning(fileName=f"{LIVE_WEBSITES_DOWNLOAD_LOCATION}/{nvalue}")
+                    except UnicodeDecodeError as error:
+                        print(f"Unicode Error - {error}")
     ### Comparison ###
+    
+    inst.comparison(productionDirectoryWalk, liveDirectoryWalk)
     
